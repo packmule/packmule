fcf1a4dad387a0ccb620ca569661cce1190d4144
Thomas Deinhamer <t.deinhamer@pixelart.at>
2018-05-28 10:27:50 +0200

    Update chunk-pack to support a runtime chunk

--- src/Packs/ChunkPack.ts
+++ src/Packs/ChunkPack.ts
@@ -5,17 +5,20 @@ import Pack from '../Core/Pack';
 interface Options {
     name: string;
     glob: string;
+    runtime: boolean;
 }
 
 export default class ChunkPack implements Pack {
     private options: Options = {
         name: '',
         glob: '',
+        runtime: false,
     };
     private configuration: webpack.Configuration;
 
-    public constructor (name: string) {
+    public constructor (name: string, runtime: boolean = false) {
         this.options.name = name;
+        this.options.runtime = runtime;
     }
 
     public include (glob: string): this {
@@ -24,19 +27,29 @@ export default class ChunkPack implements Pack {
     }
 
     public generate (): webpack.Configuration {
-       this.configuration = {
-            optimization: {
-                splitChunks: {
-                    cacheGroups: {
-                        [this.options.name]: {
-                            test: micromatch.makeRe(this.options.glob),
-                            name: this.options.name,
-                            chunks: 'all',
-                        }
+        if (this.options.runtime) {
+            this.configuration = {
+                optimization: {
+                    runtimeChunk: {
+                        name: this.options.name,
                     },
                 },
-            },
-        };
+            }
+        } else {
+            this.configuration = {
+                optimization: {
+                    splitChunks: {
+                        cacheGroups: {
+                            [this.options.name]: {
+                                test: micromatch.makeRe(this.options.glob),
+                                name: this.options.name,
+                                chunks: 'all',
+                            }
+                        },
+                    },
+                },
+            };
+        }
 
         return this.configuration;
     }
