99348b5f63d112b6075b6e2ac517555ba6f341c8
Thomas Deinhamer <t.deinhamer@pixelart.at>
2018-09-11 14:45:22 +0200

    Remove obsolete packages and typings

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,8 @@
 declare module '*.json';
 declare module 'pkg-dir';
-declare module 'zopfli-webpack-plugin';
-declare module 'brotli-webpack-plugin';
 declare module 'uglifyjs-webpack-plugin';
 declare module 'clean-webpack-plugin';
 declare module 'stylelint-webpack-plugin';
-declare module '@sucrase/webpack-object-rest-spread-plugin';
 declare module 'external-svg-sprite-loader/lib/SvgStorePlugin';
 declare module 'webpack-stylish';
 declare module 'multi-loader';
