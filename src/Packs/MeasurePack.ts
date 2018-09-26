c544ed15b109de12471a8941c0606aa150af2391
Thomas Deinhamer <t.deinhamer@pixelart.at>
2018-09-26 14:26:51 +0200

    Enhance measure pack by defining which files to include

--- src/Packs/MeasurePack.ts
+++ src/Packs/MeasurePack.ts
@@ -1,9 +1,11 @@
+import * as micromatch from 'micromatch';
 import * as webpack from 'webpack';
 import Pack from '../Core/Pack';
 
 interface Options {
     entrySize: number;
     assetSize: number;
+    glob?: string;
 }
 
 export default class MeasurePack implements Pack {
@@ -11,6 +13,7 @@ export default class MeasurePack implements Pack {
     private defaults: Options = {
         entrySize: 0,
         assetSize: 0,
+        glob: '**/*.{js,css,html,ttf,woff,woff2,otf}',
     };
 
     private configuration: webpack.Configuration = {
@@ -25,9 +28,20 @@ export default class MeasurePack implements Pack {
         this.options = {...this.defaults, ...options};
     }
 
+    public include(glob: string): this {
+        this.options.glob = glob;
+        return this;
+    }
+
     public generate(): webpack.Configuration {
         this.configuration.performance!.maxEntrypointSize = this.options.entrySize;
         this.configuration.performance!.maxAssetSize = this.options.assetSize;
+
+        if (this.options.glob) {
+            this.configuration.performance!.assetFilter = (path: string) => {
+                return micromatch.isMatch(path, this.options.glob!);
+            };
+        }
 
         return this.configuration;
     }
