15283cedcefc10812ad1790c4e154672b6058b45
Thomas Deinhamer <t.deinhamer@pixelart.at>
2019-02-09 14:35:16 +0100

    Set sane log level for the webpack dev middleware

--- src/Utility/Server.ts
+++ src/Utility/Server.ts
@@ -22,6 +22,7 @@ export default class Server {
         const dev = webpackDevMiddleware(compiler, {
             publicPath: this.webpackConfiguration.output!.publicPath || '/',
             stats: this.webpackConfiguration.stats,
+            logLevel: 'warn',
             writeToDisk: true,
             lazy: false,
         });
