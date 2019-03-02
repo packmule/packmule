2c8b182d46a1ae38a836e95a17619238853413f7
Thomas Deinhamer <t.deinhamer@pixelart.at>
2019-03-02 11:02:22 +0100

    Fix import syntax on the internal base pack

--- src/Packs/Core/BasePack.ts
+++ src/Packs/Core/BasePack.ts
@@ -1,4 +1,4 @@
-import * as webpack from 'webpack';
+import webpack from 'webpack';
 import BarPlugin from 'webpackbar';
 import Pack from '../../Core/Pack';
 import Options from '../../Core/Options';
