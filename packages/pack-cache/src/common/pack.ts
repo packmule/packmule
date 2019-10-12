dab8bd6074cd57b2d3d44d521d35f0953f5a9e55
Thomas Deinhamer <thasmo@thasmo.com>
2019-10-12 19:05:45 +0200

    Add initial version of the cache pack

---
+++ packages/pack-cache/src/common/pack.ts
@@ -0,0 +1,18 @@
+import webpack from 'webpack';
+import { Hints, Options, Pack } from '@packmule/core';
+
+export default class CachePack implements Pack {
+    private configuration: webpack.Configuration = {
+        cache: false,
+    };
+
+    public generate(options: Options, hints: Hints): webpack.Configuration {
+        if (hints.watch && hints.cache) {
+            this.configuration.cache = {
+                type: 'filesystem',
+            };
+        }
+
+        return this.configuration;
+    }
+}
---
+++ packages/pack-cache/test/pack.ts
@@ -0,0 +1,29 @@
+import test from 'ava';
+import Pack from '../src/';
+
+test('pack instantiation', (t) => {
+    const pack = new Pack();
+
+    t.truthy(pack);
+});
+
+test('cache is false by default', (t) => {
+    const pack = new Pack();
+    const configuration = pack.generate({}, {});
+
+    t.false(configuration.cache);
+});
+
+test('cache type is filesystem', (t) => {
+    const pack = new Pack();
+    const configuration = pack.generate(
+        {},
+        {
+            watch: true,
+            cache: true,
+        },
+    );
+
+    // @ts-ignore
+    t.true(configuration.cache.type === 'filesystem');
+});
