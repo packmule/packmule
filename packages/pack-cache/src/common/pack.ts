05c2831f7bb7302c2c7766a8d6e5d2591fc71c01
Thomas Deinhamer <thasmo@thasmo.com>
2019-10-28 20:47:14 +0100

    Add API to configure files invalidating the cache on change

--- packages/pack-cache/src/common/pack.ts
+++ packages/pack-cache/src/common/pack.ts
@@ -1,15 +1,39 @@
 import webpack from 'webpack';
 import { Hints, Options, Pack } from '@packmule/core';
 
+interface PackOptions {
+    dependencies?: {
+        [key: string]: string;
+    };
+}
+
 export default class CachePack implements Pack {
+    private options: PackOptions;
+    private defaults: PackOptions = {
+        dependencies: {},
+    };
+
     private configuration: webpack.Configuration = {
         cache: false,
     };
+
+    public constructor() {
+        this.options = this.defaults;
+    }
+
+    public add(path: string): void {
+        this.options.dependencies!.path = path;
+    }
+
+    public remove(path: string): void {
+        delete this.options.dependencies!.path;
+    }
 
     public generate(options: Options, hints: Hints): webpack.Configuration {
         if (hints.cache) {
             this.configuration.cache = {
                 type: 'filesystem',
+                buildDependencies: this.options.dependencies,
             };
         }
 
