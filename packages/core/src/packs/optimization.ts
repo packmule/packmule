ba5602e843a5e5792ee2a649d7a3ef02d571fc31
Thomas Deinhamer <thasmo@thasmo.com>
2019-11-03 20:40:04 +0100

    Optimize default chunk splitting options for the watch mode

--- packages/core/src/packs/optimization.ts
+++ packages/core/src/packs/optimization.ts
@@ -4,22 +4,30 @@ import Options from '../common/options';
 import Hints from '../common/hints';
 
 export default class OptimizationPack implements Pack {
-    private configuration: webpack.Configuration = {
-        optimization: {
-            splitChunks: {
-                minSize: 0,
-                minChunks: 1,
-                maxInitialRequests: Infinity,
-                maxAsyncRequests: Infinity,
-                cacheGroups: {
-                    default: false,
-                    vendors: false,
-                },
-            },
-        },
-    };
+    private configuration: webpack.Configuration = {};
 
     public generate(options: Options, hints: Hints): webpack.Configuration {
+        if (hints.watch) {
+            this.configuration.optimization = {
+                splitChunks: false,
+                removeEmptyChunks: false,
+                removeAvailableModules: false,
+            };
+        } else {
+            this.configuration.optimization = {
+                splitChunks: {
+                    minSize: 0,
+                    minChunks: 1,
+                    maxInitialRequests: Infinity,
+                    maxAsyncRequests: Infinity,
+                    cacheGroups: {
+                        default: false,
+                        vendors: false,
+                    },
+                },
+            };
+        }
+
         return this.configuration;
     }
 }
