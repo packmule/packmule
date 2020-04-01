c91c6960e387ac3b76c04b45cfa69d1dc723c4f3
Thomas Deinhamer <thasmo@thasmo.com>
2020-04-01 20:30:50 +0200

    Update clean plugin

--- src/Packs/CleanPack.ts
+++ src/Packs/CleanPack.ts
@@ -1,34 +1,18 @@
 import webpack from 'webpack';
-import CleanPlugin from 'clean-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import Pack from '../Core/Pack';
 import Options from '../Core/Options';
 
-interface CleanPackOptions {
-    path?: string;
-}
-
 export default class CleanPack implements Pack {
-    private options: CleanPackOptions;
-    private defaults: CleanPackOptions = {};
-
     private configuration: webpack.Configuration = {
         plugins: [],
     };
 
-    public constructor(path: string) {
-        this.options = {
-            ...this.defaults,
-            ...{ path },
-        }
+    public constructor(path?: string) {
     }
 
     public generate(options: Options): webpack.Configuration {
-        const settings = {
-            root: options.root,
-            verbose: options.debug && !options.watch,
-        };
-
-        const clean = new CleanPlugin([this.options.path], settings);
+        const clean = new CleanWebpackPlugin();
         this.configuration.plugins!.push(clean);
 
         return this.configuration;
