3d08218f8db5db9a27f316ec7a239f676702c61e
Thomas Deinhamer <thasmo@thasmo.com>
2020-05-13 20:28:39 +0200

    Enable relative URLs on the less loader

--- packages/pack-less/src/common/pack.ts
+++ packages/pack-less/src/common/pack.ts
@@ -63,7 +63,6 @@ export default class LessPack implements Pack {
                 loader: 'less-loader',
                 options: {
                     sourceMap: true,
-                    relativeUrls: false,
                 },
             },
         ];
