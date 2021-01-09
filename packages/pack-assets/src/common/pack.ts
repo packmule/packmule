a3273304f76c9156aa4e6833624954be95b162d4
Thomas Deinhamer <thasmo@thasmo.com>
2021-01-09 11:50:14 +0100

    Update webpack-assets-manifest

--- packages/pack-assets/src/common/pack.ts
+++ packages/pack-assets/src/common/pack.ts
@@ -2,13 +2,31 @@ import webpack from 'webpack';
 import ManifestPlugin from 'webpack-assets-manifest';
 import { Pack } from '@packmule/core';
 
+interface PackOptions {
+    name?: string;
+}
+
 export default class AssetsPack implements Pack {
+    private options: PackOptions;
+    private defaults: PackOptions = {
+        name: 'manifest.json',
+    };
+
     private configuration: webpack.Configuration = {
         plugins: [],
     };
 
+    public constructor(name?: string) {
+        this.options = this.defaults;
+
+        if (name) {
+            this.options.name = name;
+        }
+    }
+
     public generate(): webpack.Configuration {
         const manifest = new ManifestPlugin({
+            output: this.options.name,
             entrypoints: true,
             publicPath: true,
             sortManifest: false,
