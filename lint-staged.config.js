0c4adf0f5be8f4de416c3ffd0fbacf671b0c6be3
Thomas Deinhamer <thasmo@thasmo.com>
2021-07-18 12:18:26 +0200

    Update config file names

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    'packages/**': ['prettier --write'],
+};
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'lf',
+    trailingComma: 'all',
+    singleQuote: true,
+    quoteProps: 'consistent',
+    semi: true,
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
