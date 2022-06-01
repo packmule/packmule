32d06f8612da0311fdd6c158798f07cfd0f96cda
Thomas Deinhamer <thasmo@thasmo.com>
2022-06-01 16:31:08 +0200

    Use imagemin-svgo in the image-optimization pack

--- packages/pack-image-optimization/src/common/pack.ts
+++ packages/pack-image-optimization/src/common/pack.ts
@@ -48,6 +48,7 @@ export default class ImageOptimizationPack implements Pack {
                         plugins: [
                             ['mozjpeg', { quality: 80 }],
                             ['pngquant', { speed: 3, strip: true }],
+                            ['svgo', {}],
                         ],
                     },
                 },
